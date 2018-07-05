0e73b906c9ef397d90b2db3d0c7071966782a6d4
Pakapon Kaewmark <sleep191@gmail.com>
2018-07-06 01:48:07 +0700

    :recycle: Wrap Deck Create view within Tab Navigator

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
+import { StyleSheet, View, StatusBar, Platform } from 'react-native';
 import { Constants } from 'expo'
+import { FontAwesome } from '@expo/vector-icons' 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import DeckCreate from './components/DeckCreate'
 import reducers from './reducers'
-import { purple } from './utils/colors';
+import { purple, white } from './utils/colors'
+import { createBottomTabNavigator } from 'react-navigation'
+
+const Tabs = createBottomTabNavigator({
+  DeckCreate: {
+    screen: DeckCreate,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
 
 function UdaciCardStatusBar({ backgroundColor, ...props}) {
   return (
@@ -23,7 +53,7 @@ export default class App extends React.Component {
       <Provider store={store}>
         <View style={styles.container}>
           <UdaciCardStatusBar backgroundColor={purple} barStyle="light-content" />
-          <DeckCreate />
+          <Tabs />
         </View>
       </Provider>
     );
