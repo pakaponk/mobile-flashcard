a1a606ae2613083a37648e5e40575059ce61fe19
Pakapon Kaewmark <sleep191@gmail.com>
2018-07-06 14:55:59 +0700

    :art:fire: Remove unnecessary comma

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,4 @@
-import { AsyncStorage,  } from 'react-native'
+import { AsyncStorage } from 'react-native'
 import { Notifications, Permissions } from 'expo'
 
 export const NOTIFICATION_KEY = 'UdaciCard:notification'
